2e71393083b6f4877adf3ffb62dca124423c6c53
gcordner <geoffcordner@gmail.com>
2023-12-24 10:42:38 -0800

    some widget stuff. Still far from what I want

--- footer.php
+++ footer.php
@@ -9,9 +9,15 @@
 
 <footer id="colophon" class="site-footer bg-gray-50 py-12" role="contentinfo">
 	<?php do_action( 'maureen_tw_footer' ); ?>
+	<?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
+		<div class="footer-widgets container mx-auto text-center text-gray-500">
+			<?php dynamic_sidebar( 'footer-widget-area' ); ?>
+		</div>
+	<?php endif; ?>
+
 
 	<div class="container mx-auto text-center text-gray-500">
-		&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
+		&copy; <?php echo date_i18n( 'Y' ); ?> - <?php echo get_bloginfo( 'name' ); ?>
 	</div>
 </footer>
 
